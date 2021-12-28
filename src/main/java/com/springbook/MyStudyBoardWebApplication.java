1e87afdb91d586bb0982f0cd5fda9451ae80b68f
OrangeHarry <hhm12345@naver.com>
2021-12-28 14:27:13 +0900

    BoardWeb Study

---
+++ src/main/java/com/springbook/MyStudyBoardWebApplication.java
@@ -0,0 +1,13 @@
+package com.springbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudyBoardWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStudyBoardWebApplication.class, args);
+	}
+
+}
